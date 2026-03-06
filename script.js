519f11a777c2e1f95aa406b4adbb65dc963d2d98
Prof. Marcelo Oliveira <marcelo.oliveira8052668@gmail.com>
2026-03-06 10:34:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,69 +1,26 @@
+function adicionarTarefa(){
+
 let input = document.getElementById("tarefaInput")
 let lista = document.getElementById("listaTarefas")
 
-// Carregar tarefas salvas
-window.onload = function () {
-
-    let tarefas = JSON.parse(localStorage.getItem("tarefas")) || []
-
-    tarefas.forEach(function (tarefa) {
-        criarTarefa(tarefa)
-    })
-
+if(input.value === ""){
+alert("Digite uma tarefa")
+return
 }
 
-function adicionarTarefa() {
+let li = document.createElement("li")
+li.textContent = input.value
 
-    if (input.value === "") {
-        alert("Digite uma tarefa!")
-        return
-    }
+let botao = document.createElement("button")
+botao.textContent = "Excluir"
 
-    criarTarefa(input.value)
-
-    salvarTarefas()
-
-    input.value = ""
-
+botao.onclick = function(){
+li.remove()
 }
 
-function criarTarefa(texto) {
+li.appendChild(botao)
+lista.appendChild(li)
 
-    let li = document.createElement("li")
-
-    let span = document.createElement("span")
-    span.textContent = texto
-
-    // marcar concluído
-    span.onclick = function () {
-        li.classList.toggle("concluida")
-        salvarTarefas()
-    }
-
-    let botao = document.createElement("button")
-    botao.textContent = "Excluir"
-
-    botao.onclick = function (e) {
-        e.stopPropagation()
-        li.remove()
-        salvarTarefas()
-    }
-
-    li.appendChild(span)
-    li.appendChild(botao)
-
-    lista.appendChild(li)
-
-}
-
-function salvarTarefas() {
-
-    let tarefas = []
-
-    document.querySelectorAll("#listaTarefas li span").forEach(function (span) {
-        tarefas.push(span.textContent)
-    })
-
-    localStorage.setItem("tarefas", JSON.stringify(tarefas))
+input.value = ""
 
 }
